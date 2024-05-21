c5058ad3e9cce1710f06fb11d628bf29ba19bc9d
Viktors Boroviks <vboroviks@inbox.lv>
2024-05-21 22:21:04 +0300

    Terminology changed to a more neutral, accepting simmulated annealing

--- examples/max_double_array.cpp
+++ examples/max_double_array.cpp
@@ -5,15 +5,15 @@
 
 const size_t g_n_generations = 1000000;
 const size_t g_population_size = 10;
-const size_t g_individual_n_genes = 100;
+const size_t g_solution_n_data = 100;
 const size_t g_n_parents_best = 3;
-const double g_p_mutate_gene = 0.05;
+const double g_p_mutate_data = 0.05;
 const std::string g_log_filename = "max_double_array_log.csv";
 
-struct MyIndividual : varga::Individual<std::vector<double> > {
-    MyIndividual()
+struct MySolution : varga::Solution<std::vector<double> > {
+    MySolution()
     {
-        genes.resize(g_individual_n_genes);
+        data.resize(g_solution_n_data);
     }
 
     std::string str(size_t n_tabs = 0)
@@ -22,13 +22,13 @@ struct MyIndividual : varga::Individual<std::vector<double> > {
         for (size_t n = 0; n < n_tabs; n++) {
             ss << "\t";
         }
-        ss << "genes:" << std::endl;
-        for (size_t i = 0; i < genes.size(); i++) {
+        ss << "data:" << std::endl;
+        for (size_t i = 0; i < data.size(); i++) {
             for (size_t n = 0; n < n_tabs; n++) {
                 ss << "\t";
             }
-            ss << "\t[" << i << "]:" << genes[i];
-            if ((i + 1) != genes.size()) {
+            ss << "\t[" << i << "]:" << data[i];
+            if ((i + 1) != data.size()) {
                 ss << std::endl;
             }
         }
@@ -40,8 +40,8 @@ struct MyIndividual : varga::Individual<std::vector<double> > {
         std::ofstream f(filename);
         f.is_open();
         f << "i,value" << std::endl;
-        for (size_t i = 0; i < genes.size(); i++) {
-            f << i << "," << genes[i] << std::endl;
+        for (size_t i = 0; i < data.size(); i++) {
+            f << i << "," << data[i] << std::endl;
         }
     }
 
@@ -49,40 +49,40 @@ struct MyIndividual : varga::Individual<std::vector<double> > {
                    const std::function<double(void)>& rnd01)
     {
         (void)s;
-        for (auto& g : genes) {
-            g = rnd01();
+        for (auto& d : data) {
+            d = rnd01();
         }
-        _genes_changed = true;
+        _changed = true;
     }
 
-    double get_fitness(varga::Settings& s)
+    double get_value(varga::Settings& s)
     {
         (void)s;
-        if (_genes_changed) {
-            _fitness = 0;
-            for (auto& g : genes) {
-                _fitness += g;
+        if (_changed) {
+            _value = 0;
+            for (auto& d : data) {
+                _value += d;
             }
-            _genes_changed = false;
+            _changed = false;
         }
-        return _fitness;
+        return _value;
     }
 
     void crossover(varga::Settings& s,
                    const std::function<double(void)>& rnd01,
-                   varga::Individual<std::vector<double> >& parent_a,
-                   varga::Individual<std::vector<double> >& parent_b)
+                   varga::Solution<std::vector<double> >& parent_a,
+                   varga::Solution<std::vector<double> >& parent_b)
     {
         uniform_crossover(s, rnd01, parent_a, parent_b);
     }
 
     void mutate(varga::Settings& s, const std::function<double(void)>& rnd01)
     {
-        assert(genes.size() != 0);
-        if (rnd01() < s.p_mutate_gene) {
-            size_t mutation_i = rnd01() * genes.size();
-            genes[mutation_i] = rnd01();
-            _genes_changed = true;
+        assert(data.size() != 0);
+        if (rnd01() < s.p_mutate_data) {
+            size_t mutation_i = rnd01() * data.size();
+            data[mutation_i] = rnd01();
+            _changed = true;
         }
     }
 };
@@ -92,23 +92,23 @@ int main()
     varga::Settings s{g_population_size, g_n_generations};
     s.n_parents_best = g_n_parents_best;
     s.progress_update_period = 10000;
-    s.p_mutate_gene = g_p_mutate_gene;
+    s.p_mutate_data = g_p_mutate_data;
     s.log_filename = g_log_filename;
 
-    varga::StateMachine<MyIndividual> sm{s};
-    sm.init_functions = {varga::init_log<MyIndividual>,
-                         varga::randomize_next_gen<MyIndividual>};
+    varga::StateMachine<MySolution> sm{s};
+    sm.init_functions = {varga::init_log<MySolution>,
+                         varga::randomize_next_gen<MySolution>};
     sm.state_functions = {
-            varga::sort_next_gen_by_fitness<MyIndividual>,
-            varga::update_log<MyIndividual>,
-            varga::print_progress<MyIndividual>,
-            varga::change_generations<MyIndividual>,
-            varga::select_next_gen_parents<MyIndividual>,
-            varga::add_next_gen_individuals_from_crossover<MyIndividual>,
-            varga::next_gen_mutations<MyIndividual>};
-    sm.closure_functions = {varga::print_stats<MyIndividual>,
-                            varga::create_stats_file<MyIndividual>,
-                            varga::create_best_individual_csv<MyIndividual>};
+            varga::sort_next_gen_by_value<MySolution>,
+            varga::update_log<MySolution>,
+            varga::print_progress<MySolution>,
+            varga::change_generations<MySolution>,
+            varga::select_next_gen_parents<MySolution>,
+            varga::add_next_gen_solutions_from_crossover<MySolution>,
+            varga::next_gen_mutations<MySolution>};
+    sm.closure_functions = {varga::print_stats<MySolution>,
+                            varga::create_stats_file<MySolution>,
+                            varga::create_best_solution_csv<MySolution>};
     sm.run();
     return 0;
 }
